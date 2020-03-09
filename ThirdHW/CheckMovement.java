38b528c77d56c133b64e288fc2559f5b5fc5e953
Affid <delorean92@yandex.ru>
2020-03-09 17:24:23 +0300

    Third Homework

--- ThirdHW/CheckMovement.java
+++ ThirdHW/CheckMovement.java
@@ -2,17 +2,17 @@ package ThirdHW;
 
 public class CheckMovement {
 
-    public static boolean isHorseMovement(Position ... positions) throws IllegalMoveException{
-        for(int i = 1; i < positions.length; i++){
-            if(!is2and1(Math.abs(positions[i].getX()-positions[i-1].getX()),
-                    Math.abs(positions[i].getY()-positions[i-1].getY()))){
-                throw new IllegalMoveException(positions[i-1],positions[i]);
+    public static boolean isHorseMovement(Position... positions) throws IllegalMoveException {
+        for (int i = 1; i < positions.length; i++) {
+            if (!is2and1(Math.abs(positions[i].getX() - positions[i - 1].getX()),
+                    Math.abs(positions[i].getY() - positions[i - 1].getY()))) {
+                throw new IllegalMoveException(positions[i - 1], positions[i]);
             }
         }
         return true;
     }
 
-    private static boolean is2and1(int x, int y){
-        return (x==2&&y==1)||(y==2&&x==1);
+    private static boolean is2and1(int x, int y) {
+        return (x == 2 && y == 1) || (y == 2 && x == 1);
     }
 }
